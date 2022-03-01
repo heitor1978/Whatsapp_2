7493a63d50eb880eb6230aeb07c405e9d20feb47
Heitor Piva Carreira <heitorpiva015@gmail.com>
2022-03-01 15:58:01 -0300

    Foi adicionado os icones no AppBar e as Tab

--- lib/views/mensagem.view.dart
+++ lib/views/mensagem.view.dart
@@ -1,36 +1,47 @@
 import 'package:flutter/material.dart';
 
 class MensagemView extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("WhatsApp 2"),
-        backgroundColor: Color(0xff128C7E),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("WhatsApp 2"),
+          backgroundColor: Color(0xff128C7E),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+          ],
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.camera_alt),),
+              Tab(child: Text("CHATS"),),
+              Tab(child: Text("STATUS"),),
+              Tab(child: Text("LIGAÇÕES"),)
+            ]),
+        ),
+        body: Row(
+          children: [
+            Column(
+              children: [
+                
+              ],
+            ),
+            Column(
+              children: [
+                Text("Nome"),
+                Text("Ultima Mensagem")
+              ],
+            ),
+            Column(
+              children: [
+                Text("20:00"),
+                Text("2")
+              ],
+            ),
+          ]),
       ),
-      body: Row(
-        children: [
-          Column(
-            children: [
-              
-            ],
-          ),
-          Column(
-            children: [
-              Text("Nome"),
-              Text("Ultima Mensagem")
-            ],
-          ),
-          Column(
-            children: [
-              Text("20:00"),
-              Text("2")
-            ],
-          ),
-        ]),
     );
-    
   }
 }
